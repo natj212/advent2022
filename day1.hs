0fe77d306891173ac49674c663f682165f27bb1d
Natalie <devnat212@gmail.com>
2024-12-05 11:24:11 -0500

    Solved for part 2 as well, but it's slow (idc)

--- day1.hs
+++ day1.hs
@@ -11,8 +11,19 @@ invertList lst = (map (\x -> x !! 0) lst, map (\x -> x !! 1) lst)
 
 
 
+
+ -- Assume lists are sorted
+getCount :: Integer -> [Integer] -> Integer
+getCount _ [] = 0
+getCount num (x:xs)
+  | num == x = 1 + getCount num xs
+  | otherwise = getCount num xs
+
+similarityScore :: ([Integer],[Integer]) -> Integer
+similarityScore (leftLst,rightLst) = foldr (+) 0 $ map (\x -> x * getCount x rightLst) leftLst
+
 main = do
   contents <- readFile "day1a.txt"
-  print $ pairDistance $ invertList $ parseNums contents
+  print $ similarityScore $ invertList $ parseNums contents
   
   
